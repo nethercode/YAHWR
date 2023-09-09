34bf1988b37bc5f494adf966d6f2f8c57f06ade9
Nethercode <github-user@hotmail.com>
2023-09-09 14:23:31 -0500

    Create Hello program in C

---
+++ C/Hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Include the standard input/output library for console I/O
+#include <stdlib.h> // Include the standard library for exit status constants
+
+int main(void) // Define the main function, which returns an integer and takes no arguments
+{
+  puts("Hello"); // Use "puts" to output string "Hello World!" to the console
+  return EXIT_SUCCESS; // Return the exit status code (usually 0) to indicate successful program execution
+}
