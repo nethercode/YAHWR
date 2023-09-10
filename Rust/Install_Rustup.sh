bb43566cf7ff63085b386770d27db296a8156b42
Nethercode <github-user@hotmail.com>
2023-09-10 18:11:06 -0500

    Create Hello program in Rust and Python

---
+++ Rust/Install_Rustup.sh
@@ -0,0 +1,6 @@
+sudo apt install -y curl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+<<COMMENT
+1. Input '1' to proceed with default installation
+2. Restart the shell to reload PATH environment variable
+COMMENT
